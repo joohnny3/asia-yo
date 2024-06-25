9569172507b57846faf4d536d1292b01d3a28531
Joohnny <johnny31258@gmail.com>
2024-06-26 02:32:51 +0800

    feat: add CurrencyExchange feature
    - 在 CurrencyExchangeController 中新增 Response 格式。
    - 在 CurrencyExchangeController 中新增 try-catch 捕捉錯誤。

--- app/Http/Controllers/CurrencyExchangeController.php
+++ app/Http/Controllers/CurrencyExchangeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Services\CurrencyExchangeService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+use \Illuminate\Http\JsonResponse;
 
 class CurrencyExchangeController extends Controller
 {
@@ -15,15 +18,27 @@ class CurrencyExchangeController extends Controller
     {
     }
 
-    public function convert(Request $request)
+    public function convert(Request $request): JsonResponse
     {
-        $data = $request->validate([
-            'amount' => 'required|regex:/^\d{1,3}(,\d{3})*(\.\d+)?$/',
-            'source' => 'required|string',
-            'target' => 'required|string',
-        ]);
-        $result = $this->currencyExchangeService->convert($data['source'], $data['target'], $data['amount']);
+        try {
+            $data = $request->validate([
+                'amount' => 'required|regex:/^\d+(,\d{3})*(\.\d+)?$/',
+                'source' => 'required|string',
+                'target' => 'required|string',
+            ]);
 
-        dd($result);
+            $result = $this->currencyExchangeService->convert($data['source'], $data['target'], $data['amount']);
+
+            return response()->json([
+                'msg' => 'success',
+                'amount' => $result
+            ]);
+        } catch (Throwable $throwable) {
+            Log::error('Error:' . $throwable->getMessage() . ' at line:' . $throwable->getLine());
+            return response()->json([
+                'msg' => 'fail',
+                'error' => $throwable->getMessage()
+            ], 400);
+        }
     }
 }
