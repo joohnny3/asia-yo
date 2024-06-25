f2e27472eb89e99911a729dfd474cd767eda7003
Joohnny <johnny31258@gmail.com>
2024-06-26 01:20:09 +0800

    chore(currency-exchange): register AppServiceProvider  for exchangeRates

--- app/Services/CurrencyExchangeService.php
+++ app/Services/CurrencyExchangeService.php
@@ -4,5 +4,11 @@ namespace App\Services;
 
 class CurrencyExchangeService
 {
+    protected array $exchangeRates;
+
+    public function __construct(array $exchangeRates)
+    {
+        $this->exchangeRates = $exchangeRates;
+    }
 
 }
